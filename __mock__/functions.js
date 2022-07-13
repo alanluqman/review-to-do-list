37338a8def02f33802e782d10582648d2e13513f
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-07-13 17:18:28 +0100

    Adjust test files

--- __mock__/functions.js
+++ __mock__/functions.js
@@ -1,5 +1,6 @@
 export const addTask = (todo, record) => todo.push(record);
 
-export const removeTask = (todo, index) => todo.splice(index, 1);
-  
-
+export const removeTask = (todo, index) => {
+  todo.splice(index, 1);
+  return todo;
+};
--- test_folder/functions.test.js
+++ test_folder/functions.test.js
@@ -1,21 +1,18 @@
-import { addTask , removeTask} from '../__mock__/functions';
+import { addTask, removeTask } from '../__mock__/functions.js';
 
-describe('AddItem and RemoveItem from array', () => {
-test('# add one item', () => {
+describe('AddItem and RemoveItem from todo array', () => {
+  test('# add one item', () => {
     const todo = [];
     const record = {
       description: 'test value',
       completed: false,
       index: 1,
     };
-
     addTask(todo, record);
-
     expect(todo).toHaveLength(1);
+  });
 
-}); 
-
-test('# remove one item', () => {
+  test('# remove one item', () => {
     const todo = [{
       description: 'test value 1',
       completed: false,
@@ -24,17 +21,12 @@ test('# remove one item', () => {
       description: 'test value 2',
       completed: false,
       index: 1,
-    },{
+    }, {
       description: 'test value 3',
       completed: false,
       index: 2,
-    } ];
-
+    }];
     removeTask(todo, 0);
-
     expect(todo).toHaveLength(2);
-
-}); 
+  });
 });
-
-
