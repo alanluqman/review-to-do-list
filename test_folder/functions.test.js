eff3d405f553fd00aac1279bad2fb9b051c400ac
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-07-13 18:52:52 +0300

    Create test file

---
+++ test_folder/functions.test.js
@@ -0,0 +1,40 @@
+import { addTask , removeTask} from '../__mock__/functions';
+
+describe('AddItem and RemoveItem from array', () => {
+test('# add one item', () => {
+    const todo = [];
+    const record = {
+      description: 'test value',
+      completed: false,
+      index: 1,
+    };
+
+    addTask(todo, record);
+
+    expect(todo).toHaveLength(1);
+
+}); 
+
+test('# remove one item', () => {
+    const todo = [{
+      description: 'test value 1',
+      completed: false,
+      index: 0,
+    }, {
+      description: 'test value 2',
+      completed: false,
+      index: 1,
+    },{
+      description: 'test value 3',
+      completed: false,
+      index: 2,
+    } ];
+
+    removeTask(todo, 0);
+
+    expect(todo).toHaveLength(2);
+
+}); 
+});
+
+
