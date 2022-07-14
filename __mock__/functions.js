fd03e89212c861fc0d2e4cb4fe5acc245df0d25c
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-07-14 16:06:21 +0300

    Add edit and status function

--- __mock__/functions.js
+++ __mock__/functions.js
@@ -3,3 +3,11 @@ export const addTask = (todo, record) => todo.push(record);
 export const removeTask = (todo, index) => {
   todo.splice(index, 1);
 };
+
+export const editDescription = (todo, index, text) =>  todo[index].description = text;
+   
+export const status = (todo, index) => {
+    const sts = todo[index].completed;
+    if (sts) todo[index].completed= false;
+    if (!sts) todo[index].completed= true;   
+};
